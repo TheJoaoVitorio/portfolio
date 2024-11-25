0a563fad0de1d97b094207d14ae7b32fa9bf9195
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-11-24 21:52:04 -0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ export default function MainContent(){
     return(
         <div className={styles.main} >
             <div>
-                <h1 data-aos="fade-up" className={`animate__animated animate__headShake `}  >Olá, me chamo João Vitório,<br></br>tenho 20 anos e sou Full Stack Developer.</h1>
+                <h1 data-aos="fade-up" className={`animate__animated animate__headShake `}  >Olá, me chamo João Vitório,<br></br>tenho 21 anos e sou Full Stack Developer.</h1>
             </div>
         </div>
     )
