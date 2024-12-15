37980f4f7e4cddd42c2c1e14be410be8f421b0b0
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-12-15 19:40:33 -0300

    data de aniversario atualizando de forma automatizada

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,42 @@
-import styles from "../components/Main.module.css"
+import { useEffect, useState } from "react";
+import styles from "../components/Main.module.css";
 import 'animate.css';
 
-export default function MainContent(){
-    return(
-        <div className={styles.main} >
+export default function MainContent() {
+    const [idade, setIdade] = useState(0);
+
+    useEffect(() => {
+        const calcularIdade = () => {
+            const dataAtual = new Date();
+            const dataNascimento = new Date(2003, 10, 24); //o mês é 0-indexado (0 = Janeiro, 10 = Novembro)
+            let idadeAtual = dataAtual.getFullYear() - dataNascimento.getFullYear();
+
+            
+            const mesAtual = dataAtual.getMonth();
+            const diaAtual = dataAtual.getDate();
+            if (
+                mesAtual < dataNascimento.getMonth() || 
+                (mesAtual === dataNascimento.getMonth() && diaAtual < dataNascimento.getDate())
+            ) {
+                idadeAtual--;
+            }
+
+            return idadeAtual;
+        };
+
+        setIdade(calcularIdade());
+    }, []);
+
+    return (
+        <div className={styles.main}>
             <div>
-                <h1 data-aos="fade-up" className={`animate__animated animate__headShake `}  >Olá, me chamo João Vitório,<br></br>tenho 21 anos e sou Full Stack Developer.</h1>
+                <h1 
+                    data-aos="fade-up" 
+                    className={`animate__animated animate__headShake`}>
+                    Olá, me chamo João Vitório,<br />
+                    tenho {idade} anos e sou Full Stack Developer.
+                </h1>
             </div>
         </div>
-    )
+    );
 }
